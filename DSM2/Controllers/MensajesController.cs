a308a76730e2c83f1fa4c1527f894e45537a022b
Aeren Dragon <joselematesfj@gmail.com>
2018-12-18 18:28:42 +0100

    DSM2 Model Mensajes 1/3 (Faltan mas vistas y assembler)

---
+++ DSM2/Controllers/MensajesController.cs
@@ -0,0 +1,110 @@
+﻿using DSMGenNHibernate.CAD.DSM;
+using DSMGenNHibernate.CEN.DSM;
+using DSMGenNHibernate.EN.DSM;
+using DSM2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+//using System.Transactions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.ComponentModel.DataAnnotations;
+//using DotNetOpenAuth.AspNet;
+//using Microsoft.Web.WebPages.OAuth;
+//using WebMatrix.WebData;
+//using DSM2.Filters;
+
+using System.IO;
+
+namespace DSM2.Controllers
+{
+    public class MensajesController : Controller
+    {
+        // GET: Mensajes
+        public ActionResult Index()
+        {
+            MensajeCEN mensajeCEN = new MensajeCEN();
+
+            //a -1 para que de todos
+            //la interfaz tiene q pasarsele una lsita i enum
+
+
+            //// TO LIST NUESTRO
+            IEnumerable<MensajeEN> listMsg = mensajeCEN.ReadAll(0, -1).ToList();
+            return View();
+        }
+
+        // GET: Mensajes/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Mensajes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Mensajes/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Mensajes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Mensajes/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Mensajes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Mensajes/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DSM2/Models/Mensajes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DSM2.Models
+{
+    public class Mensajes
+    {
+        //LOS MODELS SON COMO LOS EN DE ESA CLASE, SOLO DECLARAS QUE VA A TENER
+        //De: https://www.tutorialspoint.com/asp.net_mvc/asp.net_mvc_scaffolding.htm
+        [ScaffoldColumn(false)]
+        public int ID { get; set; }
+
+        [Display(Prompt = "Mensaje", Description = "Mensaje", Name = "Mensaje ")]
+        [Required(ErrorMessage = "Debe indicar un mensaje")]
+        [StringLength(maximumLength: 500, ErrorMessage = "El mensaje no puede tener más de 500 caracteres")]
+        public string Mensaje { get; set; }
+
+        [ScaffoldColumn(false)]
+        public Boolean Leido { get; set; }
+    }
+}
